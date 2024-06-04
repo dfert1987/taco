d6324b865771ab7fdfa8c9e08d1dfcdbcf1b62f1
fertittad <David.Fertitta@hmhco.com>
2024-06-04 10:01:12 -0500

    basic routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+import Map from './pages/Map';
 
 function App() {
-  return (
-    <>
-    <h1>Hello World!</h1>
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/profile' element={<Profile />} />
+                    <Route path='/sign-in' element={<SignIn />} />
+                    <Route path='/sign-up' element={<SignUp />} />
+                    <Route
+                        path='/forgot-password'
+                        element={<ForgotPassword />}
+                    />
+                    <Route path='/map' element={<Map />} />
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
---
+++ src/pages/Trucks.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Trucks() {
+    return <div>Trucks</div>;
+}
