d284a3303b011fff29ad7d480e4d51c67c346ddb
fertittad <David.Fertitta@hmhco.com>
2024-06-04 22:06:15 -0500

    header component and basic routing

---
+++ src/components/Header.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import TacoLogo from '../assets/images/tacologotrans.png';
+import '../styles/mainStyle.css';
+
+export default function Header() {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const pathMatchRoute = (route) => {
+        if (route === location.pathname) {
+            return true;
+        }
+    };
+
+    return (
+        <div className='bg-lettuce border-b shadow-sm sticky top-0 z-50'>
+            <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
+                <div>
+                    <img
+                        src={TacoLogo}
+                        alt='logo'
+                        className='h-24 cursor-pointer drop-shadow-md pb-2'
+                        onClick={() => navigate('/')}
+                    />
+                </div>
+                <div>
+                    <ul className='flex space-x-10 text-white font-bebas text-3xl'>
+                        <li
+                            className={`navShadow cursor-pointer ${
+                                pathMatchRoute('/') && 'text-shellDark'
+                            }`}
+                            onClick={() => navigate('/')}>
+                            Home
+                        </li>
+                        <li
+                            className={`navShadow cursor-pointer ${
+                                pathMatchRoute('/map') && 'text-shellDark'
+                            }`}
+                            onClick={() => navigate('/map')}>
+                            Map
+                        </li>
+                        <li
+                            className={`navShadow cursor-pointer ${
+                                pathMatchRoute('/sign-in') && 'text-shellDark'
+                            }`}
+                            onClick={() => navigate('/sign-in')}>
+                            Sign In
+                        </li>
+                    </ul>
+                </div>
+            </header>
+        </div>
+    );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+           
+        },
+        colors: {
+            lettuce: '#41a332',
+            shellLight: '#f6c95c',
+            shellDark: '#e7af00',
+            salsa: '#c91919',
+            beef: '#983d00',
+            white: 'white',
+        },
+        dropShadow: {
+            nav: '0 35px 35px #c91919'
+        },
+        fontFamily: {
+            bebas: 'Bebas Neue, sans-serif',
+        }
     },
     plugins: [],
 };
