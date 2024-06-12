184edca481276a9c8bbffd32d123575d313dc6cb
fertittad <David.Fertitta@hmhco.com>
2024-06-11 19:32:52 -0500

    profile page UI

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,131 @@
-import React from 'react'
+import React, { useState } from 'react';
+import TempAv from '../assets/images/tacostand.jpeg';
+import { MdLocationCity } from 'react-icons/md';
 
 export default function Profile() {
-  return (
-    <div>
-      Profile
-    </div>
-  )
+    const [formData, setFormData] = useState({
+        name: 'David',
+        email: 'davidj.fertitta@gmail.com',
+        about: 'I sure do love tacos.',
+        age: 30,
+        locationState: 'TX',
+        locationCountry: 'USA',
+        locationCity: 'Austin',
+    });
+
+    const { name, email, about, locationCity, locationCountry, locationState } =
+        formData;
+
+    return (
+        <>
+            <section className='max-w-6xl mx-auto flex justify-center flex-col'>
+                <h1 className='text-3xl text-center mt-6 font-bold'>
+                    My Profile
+                </h1>
+                <div className='flex flex-col md:flex-row justify-around'>
+                    <div className='w-[50%] mx-auto mt-4 md:w-[30%] lg:w-[35%] mb-12 md:mb-6 rounded border-beef'>
+                        <img
+                            className='rounded border-salsa border-2 shadow-lg'
+                            src={TempAv}
+                            alt='avatar'
+                        />
+                    </div>
+                    <div className='flex-col w-full md:w-[50%] mt-6 px-3'>
+                        <form>
+                            <div className='mb-4'>
+                                <label
+                                    class='block text-sm font-bold mb-1'
+                                    for='name'>
+                                    Username:
+                                </label>
+                                <input
+                                    type='text'
+                                    id='name'
+                                    value={name}
+                                    disabled
+                                    className='w-full px-4 py-2 text-xl text-darkGray bg-white border border-beef rounded transition ease-in-out'
+                                />
+                            </div>
+                            <div className='mb-4'>
+                                <label
+                                    class='block text-sm font-bold mb-1'
+                                    for='email'>
+                                    Email:
+                                </label>
+                                <input
+                                    type='email'
+                                    id='email'
+                                    value={email}
+                                    disabled
+                                    className='w-full px-4 py-2 text-xl text-darkGray bg-white border border-salsa rounded transition ease-in-out'
+                                />
+                            </div>
+                            <div className='mb-4'>
+                                <label
+                                    class='block text-sm font-bold mb-1'
+                                    for='about'>
+                                    About:
+                                </label>
+                                <input
+                                    type='text'
+                                    id='about'
+                                    value={about}
+                                    disabled
+                                    className='w-full px-4 py-2 text-xl text-darkGray bg-white border border-beef rounded transition ease-in-out'
+                                />
+                            </div>
+                            <div className='flex justify-between'>
+                                <div className='mb-4 mr-4'>
+                                    <label
+                                        class='block text-sm font-bold mb-1'
+                                        for='locationCity'>
+                                        City:
+                                    </label>
+                                    <input
+                                        type='text'
+                                        id='locationCity'
+                                        value={locationCity}
+                                        disabled
+                                        className='w-full px-4 py-2 text-xl text-darkGray bg-white border border-beef rounded transition ease-in-out'
+                                    />
+                                </div>
+                                <div className='mb-4 mr-4'>
+                                    <label
+                                        class='block text-sm font-bold mb-1'
+                                        for='locaitonState'>
+                                        State:
+                                    </label>
+                                    <input
+                                        type='text'
+                                        id='locationCity'
+                                        value={locationState}
+                                        disabled
+                                        className='w-full px-4 py-2 text-xl text-darkGray bg-white border border-beef rounded transition ease-in-out'
+                                    />
+                                </div>
+                                <div className='mb-4'>
+                                    <label
+                                        class='block text-sm font-bold mb-1'
+                                        for='locationCountry'>
+                                        Country:
+                                    </label>
+                                    <input
+                                        type='text'
+                                        id='locationCountry'
+                                        value={locationCountry}
+                                        disabled
+                                        className='w-full px-4 py-2 text-xl text-darkGray bg-white border border-beef rounded transition ease-in-out'
+                                    />
+                                </div>
+                            </div>
+                            <div className='flex mb-6 justify-between whitespace-nowrap text-sm sm:text-lg'>
+                                <p className='text-darkLettuce hover:text-lettuce transition ease-in-out duration-200 cursor-pointer'>Update Profile</p>
+                                <p className='text-salsa hover:text-beef transition ease-in-out duration-200 cursor-pointer'>Sign Out</p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 }
